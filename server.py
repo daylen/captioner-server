c4b020d63a5c6bdc66709b3b8b274023f24424d2
Daylen Yang <services@daylenyang.com>
2016-02-17 12:24:14 -0800

    Add web API

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
+from flask import Flask, request, redirect, url_for, jsonify
+from time import time
 from captioner import Captioner
+from scipy.misc import imread
 
 weights_path = './lrcn_finetune_vgg_trainval_iter_100000.caffemodel'
 image_net_proto = './VGG_ILSVRC_16_layers_deploy.prototxt'
@@ -11,3 +14,40 @@ def get_caption(fname):
 	descriptor = c.image_to_descriptor(fname)
 	indices = c.predict_caption(descriptor)[0][0]
 	return c.sentence(indices)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return 'Image Captioning as a Service. Usage: /upload'
+
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'JPG', 'JPEG'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            start = time()
+            img = imread(file)
+            print img.shape
+            caption = get_caption(img)
+            print caption
+            print 'finished nn', time() - start
+            json = {'caption': caption, 'time': time() - start}
+            return jsonify(json)
+    return '''
+    <!doctype html>
+    <h1>Upload picture</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=False)
